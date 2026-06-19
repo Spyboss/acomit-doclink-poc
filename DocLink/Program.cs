02f2ce0f9911c67278914db9901ea1e6e6243e69
DocLink Dev <dev@acomit.com>
2026-06-19 11:13:28 +0530

    add Dockerfile, .dockerignore, configure PORT env for Railway

--- DocLink/Program.cs
+++ DocLink/Program.cs
@@ -8,6 +8,9 @@ using DocLink.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 QuestPDF.Settings.License = LicenseType.Community;
 
 builder.Services.AddControllersWithViews();
